82ab23d071d4cab065e69e255d67a424e083492c
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-04-14 04:48:33 +0530

    new improved build system

--- 001-rekt/lua/src/main.lua
+++ 001-rekt/lua/src/main.lua
@@ -1,64 +1,8 @@
 --[[
---  dh-fastest-code-content
---]]
+  dh-fastest-code-content
+]]
 
-local utils = {}
-
---[[
---  not the best algorithm to split a string
---]]
-utils.split = function(str, delim)
-
-    assert(str ~= nil)
-
-    delim = delim or " "
-
-    local chunks = {}
-    local last_idx = 0
-    for idx = 1, #str do
-        local chr = str:sub(idx, idx)
-        if chr == delim then
-            local x = str:sub(last_idx+1, idx-1)
-            last_idx = idx
-            table.insert(chunks, x)
-        end
-    end
-
-    if last_idx ~= #str then
-        table.insert(chunks, str:sub(last_idx))
-    end
-
-    return chunks
-end
-
-utils.tostring = function(entity)
-
-    if type(entity) ~= "table" then
-        -- this is the builtin tostring
-        return tostring(entity)
-    end
-
-    local str = "{"
-    for key, value in pairs(entity) do
-        str = str .. string.format(" %s = %s", key, utils.tostring(value)) .. ","
-    end
-
-    if #str > 1 then
-        str = str:sub(1, #str-1) .. " }"
-    else
-        str = str .. " }"
-    end
-
-    return str
-end
-
-utils.slice = function(table, start, last)
-    local sub = {}
-    for i = start, last do
-        table.insert(sub, table[i])
-    end
-    return sub
-end
+local utils = require "utils.lua"
 
 local exports = {}
 local internals = {
